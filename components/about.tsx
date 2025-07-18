db7de240c006d55fccde543a6a2a2d65f0a7351d
iaffansheikh21 <muhammadaffansheikh21@gmail.com>
2025-07-18 15:56:33 +0500

    intro updated

--- components/about.tsx
+++ components/about.tsx
@@ -19,20 +19,20 @@ export default function About() {
       <SectionHeading title="About Me" />
       <p className="mb-3">
         I have completed my{" "}
-<span className="font-medium">Bachelor of Science in Software Engineering</span>. 
-With a passion for programming, I remain consistently eager and enthusiastic about 
-acquiring new knowledge. I have gained expertise in{" "}
-<span className="font-medium">full-stack web development</span>.{" "}
-<span className="italic">My favorite part of programming</span> is the 
-problem-solving aspect. I <span className="underline">love</span> the 
-feeling of finally figuring out a solution to a problem. My core stack is{" "}
-<span className="font-medium">
-  React, Next.js, Node.js, and MongoDB
-</span>
-. I am also familiar with C++, TypeScript, and MySQL. I am always 
-looking to learn new technologies. I am currently seeking a{" "}
-<span className="font-medium">full-time opportunity</span> as a software 
-developer.
+        <span className="font-medium">Bachelor of Science in Software Engineering</span>.
+        With a passion for programming, I remain consistently eager and enthusiastic about
+        acquiring new knowledge. I have gained expertise in{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack is{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, and MongoDB
+        </span>
+        . I am also familiar with C++, TypeScript, and MySQL. I am always
+        looking to learn new technologies. I am currently seeking a{" "}
+        <span className="font-medium">full-time opportunity</span> as a software
+        developer.
 
       </p>
     </motion.section>
